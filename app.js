71577cf1f2ed812edea035a44c4d5eb632fbf556
michael floresca <michaelfloresca88@gmail.com>
2022-08-31 16:05:08 -0700

    updating site

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('hello world!');
+
+let usersName = prompt('What is your name?');
+let message;
+
+
+if (usersName == 'Kassie'){
+    message = 'HI TEACHER KASSIE!'
+  } else if (usersName == 'Hunter') {
+    message = "Sup Hunter!";
+  } else if (usersName == 'Mike'){
+    message = "Hi Mike!";
+  } else {
+    message = 'Thank you for checking out my site!';
+  }

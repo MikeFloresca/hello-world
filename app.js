32d083d0d430ffa5638ee8c98645a3cce740c9fa
michael floresca <michaelfloresca88@gmail.com>
2022-09-02 14:16:02 -0700

    added stuff

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log('fishing');
 
 function showBasketball(){
   let usersNum =prompt('Blindly rate my page?1-5');
-  
+ 
   for (let i =0; i < usersNum && i < 5; i++){
 
     // document.write('<img src="https://c.tenor.com/wCUl5V7vzHwAAAAC/friends-dont-lie-promise.gif" style="width:50px;"/>');
@@ -35,6 +35,17 @@ if (usersHobby == 'Basketball'){
 }
 
 // greetUsers();
+
+
+
+function pickNum(){
+  let userNumber = prompt("Pick a number between 1-3" );
+  while (userNumber !== '2'){
+    alert('nope that is the wrong number')
+    userNumber = prompt("Pick a number between 1-3" );
+  }
+  alert('Great job!!!')
+}
 
 function cyalater(){
 document.write('Thanks for checking me out!');
